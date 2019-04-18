ab978babe92b2047d2ac9493f60265fc4b1d2da5
rodgef <rodgef101@gmail.com>
2019-04-18 11:16:18 +0200

    example usage file

--- htmlWrapper.php
+++ htmlWrapper.php
@@ -1,8 +1,18 @@
 <?php
 
+ include('./tag.php');
+
  define("NEWLINE", "<br>" ); 
  define("NO_INFO","no extra") ; 
  define("DOCTYPE","<!DOCTYPE html>");
 
+
+ $html = Tag('html',true);
+ $head = Tag('head',true);
+ $body = Tag('body',true);
+ 
+ $html->add_body($head,false);
+ $html->commit();
+ echo $html->display();
 
 ?>
